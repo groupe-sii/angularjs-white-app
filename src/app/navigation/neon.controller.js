30861d1af1e6644f9bb18675188c807f431a3356
Valentin GOT <vgot@sii.fr>
2015-10-21 11:50:59 +0200

    docs(Neon navigation): add neon navigation documentation

--- src/app/navigation/neon.controller.js
+++ src/app/navigation/neon.controller.js
@@ -5,12 +5,28 @@
         .module('angularjsWhiteApp')
         .controller('NavigationNeonController', NavigationNeonController);
 
-    NavigationNeonController.$inject = [];
-
     /* @ngInject */
     function NavigationNeonController() {
         var vm = this;
 
-        vm.navigation = [];
+        vm.navigation = [
+            {
+                name: '<i class="fa fa-home"></i><span class="title">Home',
+                link: 'app.home'
+            },
+            {
+                name: '<i class="fa fa-bars"></i><span class="title">Navigation</span>',
+                subtree: [
+                    {
+                        name: '<span class="title">Bootstrap</span>',
+                        link: 'app.navigation.bootstrap'
+                    },
+                    {
+                        name: '<span class="title">Neon</span>',
+                        link: 'app.navigation.neon'
+                    }
+                ]
+            }
+        ];
     }
 })();
