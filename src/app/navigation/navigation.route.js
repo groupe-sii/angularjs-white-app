fa5731db3f8df4839e5296b12b61b1d3ce33fe8c
Valentin GOT <vgot@sii.fr>
2015-10-21 08:00:51 +0200

    feat(Navigation): initialize navigation routes and templates

---
+++ src/app/navigation/navigation.route.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('angularjsWhiteApp')
+        .config(config);
+
+    /* @ngInject */
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('app.navigation', {
+                abstract: true,
+                url: 'navigation/',
+                templateUrl: 'app/navigation/navigation.html'
+            })
+
+            .state('app.navigation.bootstrap', {
+                url: 'bootstrap',
+                templateUrl: 'app/navigation/bootstrap.html',
+                controller: 'NavigationBootstrapController',
+                controllerAs: 'b'
+            })
+
+            .state('app.navigation.neon', {
+                url: 'neon',
+                templateUrl: 'app/navigation/neon.html',
+                controller: 'NavigationNeonController',
+                controllerAs: 'n'
+            });
+
+        $urlRouterProvider.otherwise('/');
+    }
+})();
