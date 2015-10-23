bb5a0df472d94a441c89fc588a0e9a17948f3da0
Valentin GOT <vgot@sii.fr>
2015-10-23 06:20:00 +0200

    refactor(Navigation): remove unused $urlRouterProvider and change app-titles from h3 to h1

--- src/app/navigation/navigation.route.js
+++ src/app/navigation/navigation.route.js
@@ -6,7 +6,7 @@
         .config(config);
 
     /* @ngInject */
-    function config($stateProvider, $urlRouterProvider) {
+    function config($stateProvider) {
         $stateProvider
             .state('app.navigation', {
                 abstract: true,
