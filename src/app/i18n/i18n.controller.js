673882f5f95172ef7cf244e345d976895a252e8a
Valentin GOT <vgot@sii.fr>
2015-11-09 11:40:09 +0100

    feat(I18N): create i18n explanations

---
+++ src/app/i18n/i18n.controller.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('angularjsWhiteApp')
+        .controller('I18NController', I18NController);
+
+    /* @ngInject */
+    function I18NController() {}
+})();
---
+++ src/app/i18n/i18n.route.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('angularjsWhiteApp')
+        .config(config);
+
+    config.$inject = ['$stateProvider'];
+
+    /* @ngInject */
+    function config($stateProvider) {
+        $stateProvider
+            .state('app.i18n', {
+                url: 'i18n',
+                templateUrl: 'app/i18n/i18n.html',
+                controller: 'I18NController',
+                controllerAs: 'i'
+            })
+
+            .state('app.i18n-static', {
+                url: 'i18n/static',
+                templateProvider: function(TranslationFactory) {
+                    return TranslationFactory.get('app/i18n/i18n-static.${LANG}.html').then(function(tpl) {
+                        return tpl;
+                    });
+                }
+            });
+    }
+})();
